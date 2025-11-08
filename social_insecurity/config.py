4df54037f0bb2bd188ad6fa35b36a00d10263526
KristianN55 <narten13@gmail.com>
2025-11-08 19:08:35 +0100

    config max file size and type

--- social_insecurity/config.py
+++ social_insecurity/config.py
@@ -20,5 +20,6 @@ class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "secret"  # TODO: Use this with wtforms
     SQLITE3_DATABASE_PATH = "sqlite3.db"  # Path relative to the Flask instance folder
     UPLOADS_FOLDER_PATH = "uploads"  # Path relative to the Flask instance folder
-    ALLOWED_EXTENSIONS = {}  # TODO: Might use this at some point, probably don't want people to upload any file type
-    WTF_CSRF_ENABLED = True  # TODO: I should probably implement this wtforms feature, but it's not a priority
+    ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+    WTF_CSRF_ENABLED = False  
+    MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # 16MB limit
